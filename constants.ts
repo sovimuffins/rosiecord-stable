3c59c12c7bf3a9e0e3ff21127916df14e28d9646
sovimuffins <sovimuffins@gmail.com>
2024-01-22 16:08:19 -0500

    HOPEFULLY 211 DOESN'T BREAK THE FUCKING APP

--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_212.0.ipa";
+    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_211.0.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
