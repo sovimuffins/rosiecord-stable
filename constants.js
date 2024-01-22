cbf6c7f7a88c43247a7f4fe5e13572462c7121b3
sovimuffins <sovimuffins@gmail.com>
2024-01-21 22:38:54 -0500

    GIF ISSUE FINALLY FIXED
    AMEN

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_212.0_53913.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_214.0_54355.ipa";
 export { Shell, Colors, Divider, States, Constants };
