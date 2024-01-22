7961059bc8d2e7bbfd02a3a6c5eff59d41c81b00
sovimuffins <sovimuffins@gmail.com>
2024-01-21 23:55:23 -0500

    grandma testflight
    lmao

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_214.0_54355.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_195.0_49168.ipa";
 export { Shell, Colors, Divider, States, Constants };
