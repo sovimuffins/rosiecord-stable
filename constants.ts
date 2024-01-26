a365ceb3141433017f21ef1d0ffe505bb2d11a32
sovimuffins <sovimuffins@gmail.com>
2024-01-25 22:39:12 -0500

    updating it to 213.1 probably a hotfix for that media issue

--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_213.0.ipa";
+    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_213.1.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
