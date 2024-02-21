1554a58eaee4f423fc48f80949a2f832262d135f
Rosella <sovimuffins@gmail.com>
2024-02-21 08:27:50 -0500

    whomp

--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_216.0.ipa";
+    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_217.0.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
