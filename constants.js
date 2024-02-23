94626c58008ac45385d5db849d73ba2c0bba5296
Rosella <sovimuffins@gmail.com>
2024-02-23 10:15:32 -0500

    building older versions people might like

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_190.0.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_200.0.ipa";
 export { Shell, Colors, Divider, States, Constants };
--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_190.0.ipa";
+    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_200.0.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
